13c93108ebcbf0da9caee13c30bc58c0aee5573c
annie guo <annie.guo.2011@gmail.com>
2014-07-02 01:38:51 +0000

    install nodejs manually

--- 01_setup.sh
+++ 01_setup.sh
@@ -3,7 +3,8 @@ echo "==Begin"
 
 echo " "
 echo "==1. install git node npm"
-sudo apt-get install git node npm
+sudo apt-get update
+sudo apt-get install git nodejs npm
 
 echo " "
 echo "==2. npm version"
@@ -11,3 +12,5 @@ npm version
 
 echo " "
 echo "==Completed successfully"
+
+# runtime, nodejs set to manually installed.
---
+++ 02_nodejs.sh
@@ -0,0 +1,13 @@
+# 1. Install the dependencies:
+sudo apt-get install g++ curl libssl-dev apache2-utils
+sudo apt-get install git-core
+
+# 2. Run the following commands:
+git clone git://github.com/ry/node.git
+cd node
+./configure
+make
+sudo make install
+
+# ref http://howtonode.org/how-to-install-nodejs
+# Ubuntu
