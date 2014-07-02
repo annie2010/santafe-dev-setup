89429a63d8e0a217f092a319325e23d15749f25c
annie guo <annie.guo.2011@gmail.com>
2014-07-02 01:06:42 +0000

    install git node npm

---
+++ 01_setup.sh
@@ -0,0 +1,13 @@
+#
+echo "==Begin"
+
+echo " "
+echo "==1. install git node npm"
+sudo apt-get install git node npm
+
+echo " "
+echo "==2. npm version"
+npm version
+
+echo " "
+echo "==Completed successfully"
