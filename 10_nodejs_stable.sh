d6f62b083c138ea5c18b8fe398b3d05ca57e85b6
annie guo <annie.guo.2011@gmail.com>
2014-09-06 06:34:55 +0000

    install nodejs stable version

---
+++ 10_nodejs_stable.sh
@@ -0,0 +1,9 @@
+wget http://nodejs.org/dist/v0.10.17/node-v0.10.17.tar.gz
+tar -xvf node-v0.10.17.tar.gz
+cd node-v0.10.17
+
+./configure
+make
+sudo make install
+
+#http://blog.nodejs.org/2013/08/21/node-v0-10-17-stable/
